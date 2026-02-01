ab244c6070fb905ae0b49bf9a00e7d0f7f28a1e0
Sarvar8 <sarvar.urinboev8@gmail.com>
2026-02-02 05:08:48 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+// import client from "@/apollo/client";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
+import client from "@/libs/apollo/client";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -13,9 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Socket.io, Redux, Mui, Apollo Client ...
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
-import Advertisement from "@/libs/components/homepage/Advertisement";
-import PopularProperties from "@/libs/components/homepage/PopularProperties";
-import TopAgents from "@/libs/components/homepage/TopAgents";
-import TopProperties from "@/libs/components/homepage/TopProperties";
-import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
 import { NextPage } from "next";
+import { useQuery } from "@apollo/client";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+// import { GET_PROPERTIES } from "@/apollo/user/query";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { GET_PROPERTIES } from "@/libs/apollo/user/query";
+
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData =>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack> Home page mobile</Stack>;
@@ -26,7 +51,7 @@ const Home: NextPage = () => {
         <TopAgents />
       </Stack>
     );
-  };
+  }
 };
 
 export default withLayoutMain(Home);
